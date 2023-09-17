c1bbd45fd8a8f609242cc47adb3580bf8c8b6645
vindhyajee5 <vindhyajee5@gmail.com>
2023-09-17 14:29:54 +0530

    test

---
+++ src/main/java/com/vindhya/SpringBootMavenApplication.java
@@ -0,0 +1,28 @@
+package com.vindhya;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootMavenApplication {
+	Logger logger = LoggerFactory.getLogger(SpringBootMavenApplication.class);
+	@RequestMapping("/")
+    String hello() {
+		logger.trace("A TRACE Hello World, Spring Boot!");
+        logger.debug("A DEBUG Hello World, Spring Boot!");
+        logger.info("An INFO Hello World, Spring Boot!");
+        logger.warn("A WARN Hello World, Spring Boot!");
+        logger.error("An ERROR Hello World, Spring Boot!");
+        return "Hello World, Spring Boot!";
+    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMavenApplication.class, args);
+	}
+
+}
