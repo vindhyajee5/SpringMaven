9dc2ecbadc92982e4f904e6acf94e500a5fcd733
vindhyajee5 <86296127+vindhyajee5@users.noreply.github.com>
2023-10-06 15:08:49 +0530

    Update SpringBootMavenApplicationTests.java

--- src/test/java/com/vindhya/SpringBootMavenApplicationTests.java
+++ src/test/java/com/vindhya/SpringBootMavenApplicationTests.java
@@ -1,13 +1,13 @@
 package com.vindhya;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class SpringBootMavenApplicationTests {
+//@SpringBootTest
+//class SpringBootMavenApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
-}
+//}
